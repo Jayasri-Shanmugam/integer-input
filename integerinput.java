e2ac4ce1e86f803c835abb38dd3b737be60dcb03
Jayasri-Shanmugam <138643993+Jayasri-Shanmugam@users.noreply.github.com>
2023-07-15 18:51:19 +0530

    Add files via upload

---
+++ integerinput.java
@@ -0,0 +1,17 @@
+package Jayasrijavapractice;
+
+import java.util.Scanner;
+
+public class integerinput {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		
+		Scanner integer=new 	Scanner(System.in);
+		int number=integer.nextInt();
+		System.out.println(number);
+		integer.close();
+	}
+
+}
